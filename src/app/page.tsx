f7eef316fe76875bd1fa5b6b814abf49d220e61c
noat <auttyzaza@gmail.com>
2023-12-27 01:44:03 +0700

    image generation is back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 export default function Home() {
   return (
     <main style={{backgroundImage:`url("bg.png")`, backgroundSize: "cover",backgroundRepeat:"no-repeat"}} className="flex min-h-screen flex-col  bg-zinc-950 space-y-10">
@@ -12,7 +14,13 @@ export default function Home() {
       - ตัวโชว์หรือเหี้ยไรสักอย่าง
       */}
       <div className='flex bg-opacity-40 outline outline-1 outline-zinc-700 shadow-medium bg-zinc-900 backdrop-blur-lg min-w-min space-x-10 space-y-10 px-52'>
-        <ul className='space-y-5'>
+          <Image
+            src="/vol.jpg"
+            width={300}
+            height={200}
+            alt="wtf"
+          />
+          <ul className='space-y-5'>
           <p className='text-6xl'>Bocchi the Rock!</p>
           <p className='text-3xl'>Genre: Comedy/Slice of Life</p>
           <p className="text-xl">mee text na jar</p>
